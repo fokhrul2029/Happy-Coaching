128541e5f89ecfa56154f930de2f9eac2fe9d826
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-02-02 02:35:06 +0600

    added js for hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
+// Element Selector
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
 
-
-var person = {};
-person.name = "John";
-person.age = 30;
-
-console.log(person.name)
+hamburger.addEventListener("click", () => {
+  if (menu.style.opacity == "0") {
+    menu.style.opacity = "1";
+    menu.style.transform = "rotatex(0deg)";
+    hamburger.style.background = "#20ad96";
+    hamburger.style.color = "white";
+  } else {
+    menu.style.opacity = "0";
+    menu.style.transform = "rotatex(90deg)";
+    hamburger.style.background = "transparent";
+    hamburger.style.color = "#20ad96";
+  }
+});
